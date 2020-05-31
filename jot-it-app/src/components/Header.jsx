a2c14d1eef8b677421d7e08a8784dbc6221921ee
osaro2020 <58718596+osaro2020@users.noreply.github.com>
2020-05-31 16:52:18 -0400

    Add files via upload

---
+++ jot-it-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NoteIcon from "@material-ui/icons/Note";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteIcon />
+        Jot-It
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
